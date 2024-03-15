fe28c2a111f5e35df41ac5ca38d1bccf4cc4109c
74Emmanuel47 <chinibos@gmail.com>
2024-03-15 15:46:37 -0600

    First Commit

---
+++ PlayerUI/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayerUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            customizeDesign();
+        }
+
+        private void customizeDesign()
+        {
+            panelMediaSubMenu.Visible = false;
+            panelPlayListSubMenu.Visible = false;
+            panelToolSubMenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            panelMediaSubMenu.Visible = false;
+            panelPlayListSubMenu.Visible = false ;
+            panelToolSubMenu.Visible = false ;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if(!subMenu.Visible)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            } else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        #region Media acctions space
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelMediaSubMenu);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Form3());
+
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+        #endregion
+
+        #region Play List actions-space
+        private void btnPlayList_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelPlayListSubMenu);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        #endregion
+
+        #region Tools actions space
+        private void btnTools_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelToolSubMenu);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+        #endregion
+
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+    }
+}
